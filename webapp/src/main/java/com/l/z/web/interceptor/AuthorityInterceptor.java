125c1dd65875dd0e3186ad0b2b24a32692709209
liuyunming <liuyunming@NB1-LIUYUNMING.hikvision.com>
2017-09-14 17:07:49 +0800

    add spring redis and spring session

--- webapp/src/main/java/com/l/z/web/interceptor/AuthorityInterceptor.java
+++ webapp/src/main/java/com/l/z/web/interceptor/AuthorityInterceptor.java
@@ -5,17 +5,23 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthorityInterceptor extends HandlerInterceptorAdapter {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
                     ModelAndView modelAndView) throws Exception {
         if (modelAndView == null) {
             return;
         }
+        
         // HttpSession session = request.getSession();
         // Object attribute = session.getAttribute("user");
         // if (attribute == null) {
